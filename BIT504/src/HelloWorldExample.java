bdf12e8509f8f184a3f69c5abcaf35db3de11d0c
Yulia Hodgson <yuliahodgson@gmail.com>
2023-12-13 14:57:03 +1300

    Test

---
+++ BIT504/src/HelloWorldExample.java
@@ -0,0 +1,12 @@
+public class HelloWorldExample {
+
+     public static void main(String[] args) {
+ 
+         // Output Hello World to the user
+ 
+         System.out.println("Hello World");
+ 
+     }
+ 
+ }
+
